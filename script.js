88c71765594d75828b624bd07d47cf6cec953e28
Tapooah <bilyk.po@gmail.com>
2021-10-06 15:10:45 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+// (function() {
+//     'use strict';
+//     var a = window.b = 5;
+// })();
+//
+// console.log(b);
+// console.log(null === null)
+// var a = { b : "c" };
+// console.log(typeof a);
+// console.log(typeof typeof 2);
+// console.log(false + false);
+// console.log(typeof null);
+// console.log(1 + '2' + 3)
+// var digitsArray = [1,2,3];
+//
+//
+// Array.prototype.each = function() {}
+// var result = '';
+// for (var digit in digitsArray) {
+//     result += digit;
+// }
+// console.log(result);
+//
+//
+// var a = new Array(5,50);
+// var b = new Array(5,50);
+// console.log(a == b);
+//
+//
+// var message = 'H';
+// console.log(message.concat('n'));
+//
+//
+// console.log(new String('‘Yellow’') === '‘Yellow’');
+//
+//
+// var userArray = new Array(3);
+// console.log(userArray.toString());
+//
+//
+// console.log(Math.min(null,200));
+//
+//
+// var digitArray = [];
+// digitArray[5] = 10;
+// console.log(digitArray.length);
+//
+//
+// console.log(11111111111111111 === 11111111111111113);
+//
+//
+// // console.Log('Green');
+//
+//
+// console.log(0.7 + 0.2);
+//
+//
+// console.log(0 ? 0 : 1);
+//
+//
+// var numbers = Array(2,10);
+// numbers[numbers.length] = 0;
+// console.log(numbers.length);
+//
+//
+// console.log(NaN == NaN || NaN === NaN);
+//
+//
+//
+// sayHi();
+// function sayHi() {
+//     alert("Hello");
+// }
+//
+//
+//
+// function User() { }
+// User.prototype = { admin: false };
+// let user = new User();
+// alert(user.admin);
+//
+//
+// function f() {
+//     let a = 5;
+//     return new Function('b', 'return a + b');
+// }
+//
+// alert( f()(1) );
+//
+//
+// // let a = (1,5 - 1) * 2;
+// //
+// // alert(a);
+//
+//
+// let arr = [1, 2, 3];
+// arr.something = 5;
+// alert(arr.something); // ?
+//
+//
+// // for(let i=0; i<10; i++) {
+// //     setTimeout(function() {
+// //         alert(i);
+// //     }, 100);
+// // }
+//
+//
+// function F() { return F; }
+//
+// alert( new F() instanceof F );
+// alert( new F() instanceof Function );
+
+// let a = new Array(1,2), b = new Array(3);
+// alert(a[0] + b[0]);
+
+// "use strict";
+// a = null + undefined;
+// alert(a);
+
+elem.innerHTML
